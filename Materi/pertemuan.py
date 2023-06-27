bbeb05b1c8a4f51c609bf10bca6fcac36b5382d8
FanoDre <fanodre@gmail.com>
2023-06-27 20:39:43 +0700

    27/6/23

--- Materi/pertemuan.py
+++ Materi/pertemuan.py
@@ -4,7 +4,7 @@ class Pertemuan1():
   def soal():
     url = 'https://docs.google.com/presentation/d/1EJvzcJguja5QW93CDL11z4UCCoqhglax/edit#slide=id.p4'
     urlOpen = webbrowser.open_new_tab(url)
-    return "Link Berhasil Dibuka", urlOpen
+    return urlOpen
   
   def tugas():
     return "Belum ada tugas di Pertemuan 1"
@@ -13,7 +13,7 @@ class Pertemuan2():
   def soal():
     url = 'https://docs.google.com/presentation/d/1DnPPbiXLGKtH9NKSfNE480bOOdTgUNjZ/edit#slide=id.p1'
     urlOpen = webbrowser.open_new_tab(url)
-    return "Link Berhasil Dibuka", urlOpen
+    return urlOpen
   
   def tugas():
     return "Belum ada tugas di Pertemuan 2"
@@ -22,7 +22,7 @@ class Pertemuan3():
   def soal():
     url = 'https://docs.google.com/presentation/d/1HrdjC5wa322m8w3wzQhoY7gwbYfwn3F1/edit#slide=id.p1'
     urlOpen = webbrowser.open_new_tab(url)
-    return "Link Berhasil Dibuka", urlOpen
+    return urlOpen
   
   def tugas():
     return "Belum ada tugas di Pertemuan 3"
@@ -31,7 +31,7 @@ class Pertemuan4():
   def soal():
     url = 'https://docs.google.com/presentation/d/1WRg4ECmmL-d-A7peoUrBfp7f79C1KVF5/edit#slide=id.p1'
     urlOpen = webbrowser.open_new_tab(url)
-    return "Link Berhasil Dibuka", urlOpen
+    return urlOpen
   
   def tugas():
     return "Belum ada tugas di Pertemuan 4"
@@ -40,7 +40,7 @@ class Pertemuan5():
   def soal():
     url = 'https://docs.google.com/presentation/d/17MzdN701NeVnfeV_OVlvIkqizK1TLV8w/edit#slide=id.p1'
     urlOpen = webbrowser.open_new_tab(url)
-    return "Link Berhasil Dibuka", urlOpen
+    return urlOpen
   
   def tugas():
     return "Belum ada tugas di Pertemuan 5"
@@ -49,7 +49,74 @@ class Pertemuan6():
   def soal():
     url = 'https://docs.google.com/presentation/d/1WzRP3NYJpLjS6yL_JvX6scHKM-BpiNMg/edit#slide=id.p1'
     urlOpen = webbrowser.open_new_tab(url)
-    return "Link Berhasil Dibuka", urlOpen
+    return urlOpen
   
   def tugas():
     return "Belum ada tugas di Pertemuan 6"
+
+class Pertemuan7():
+  def soal():
+    url = 'https://drive.google.com/drive/folders/1DKjopSv_6Ardf0KBkbTD4nzTD0LeDHL7?usp=sharing'
+    urlOpen = webbrowser.open_new_tab(url)
+    return urlOpen
+  
+  def tugas():
+    return "Belum ada tugas di Pertemuan 7"
+  
+  
+class Pertemuan8():
+  def soal():
+    url = 'https://drive.google.com/drive/folders/10EFGudAzJqRTWP8fxjfXRWA1hDj5sb5z?usp=sharing'
+    urlOpen = webbrowser.open_new_tab(url)
+    return urlOpen
+  
+  def tugas():
+    return "Belum ada tugas di Pertemuan 8"
+  
+  
+class Pertemuan9():
+  def soal():
+    url = 'https://docs.google.com/presentation/d/1kuUJ_5uvXuQJgQEq2ukCkIzMxHStTBwe/edit?rtpof=true&sd=true'
+    urlOpen = webbrowser.open_new_tab(url)
+    return urlOpen
+  
+  def tugas():
+    return "9"
+  
+class Pertemuan10():
+  def soal():
+    url = 'https://docs.google.com/presentation/d/10iJvb04ifrSuhQp9Pkh5OfB1uzSq0sa6/edit'
+    urlOpen = webbrowser.open_new_tab(url)
+    return urlOpen
+  
+  def tugas():
+    return "10"
+  
+class Pertemuan11():
+  def soal():
+    url = 'https://docs.google.com/presentation/d/1tBn4hqI6i9mx_GttrMo2hgVmf0E5bZb1/edit?rtpof=true&sd=true'
+    urlOpen = webbrowser.open_new_tab(url)
+    return urlOpen
+  
+  def tugas():
+    return "11"
+  
+class Pertemuan12():
+  def soal():
+    url = 'https://docs.google.com/presentation/d/1_PQ9iy1M_K1kYOiXq2OG4aHQrD78XtcH/edit#slide=id.p1'
+    urlOpen = webbrowser.open_new_tab(url)
+    return urlOpen
+  
+  def tugas():
+    return "12"
+  
+class Pertemuan13():
+  def soal():
+    url = 'https://docs.google.com/presentation/d/1JWq9wcUBtHs8YGhNDlz5499SB0wAoR0C/edit'
+    urlOpen = webbrowser.open_new_tab(url)
+    return urlOpen
+  
+  def tugas():
+    return "13"
+  
+  
--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import os
 from Materi.pertemuan import *
 os.system('cls')
 
-menu = ['Pertemuan 1', 'Pertemuan 2', 'Pertemuan 3',
-        'Pertemuan 4', 'Pertemuan 5', 'Pertemuan 6']
+menu = [' Pertemuan 1', ' Pertemuan 2', ' Pertemuan 3',
+        ' Pertemuan 4', ' Pertemuan 5', ' Pertemuan 6',
+        ' Pertemuan 7', ' Pertemuan 8', ' Pertemuan 9', 
+        'Pertemuan 10', 'Pertemuan 11', 'Pertemuan 12',
+        'Pertemuan 13']
 
 menus = """MENU:
       1. Lihat Materi
@@ -24,10 +27,11 @@ while True:
     
   print(50*'=')
   pilihMenu = input("Pilih Menu 1-13 [Q:Exit]: ").lower()
-  print(50*'=')
   
   if pilihMenu == "1":
     while True:
+      os.system('cls')
+      print(50*'=')
       print("\t\t  (Pertemuan 1)")
       print("\t     Pengenalan Dasar Python")
       print(50*'-')
@@ -49,6 +53,8 @@ while True:
     
   elif pilihMenu == "2":
     while True:
+      os.system('cls')
+      print(50*'=')
       print("\t\t  (Pertemuan 2)")
       print("\t     Pengenalan Dasar Python")
       print(50*'-')
@@ -70,6 +76,8 @@ while True:
       
   elif pilihMenu == "3":
     while True:
+      os.system('cls')
+      print(50*'=')
       print("\t\t  (Pertemuan 3)")
       print("Python Data Types, Variables, Operators, and Basic I/O Operations")
       print(50*'-')
@@ -91,6 +99,8 @@ while True:
       
   elif pilihMenu == "4":
     while True:
+      os.system('cls')
+      print(50*'=')
       print("\t\t  (Pertemuan 4)")
       print("\t    Output Program pada Python")
       print(50*'-')
@@ -112,6 +122,8 @@ while True:
       
   elif pilihMenu == "5":
     while True:
+      os.system('cls')
+      print(50*'=')
       print("\t\t  (Pertemuan 5)")
       print("  Boolean Values, Conditional Execution Continues")
       print(50*'-')
@@ -133,6 +145,8 @@ while True:
   
   elif pilihMenu == "6":
     while True:
+      os.system('cls')
+      print(50*'=')
       print("\t\t  (Pertemuan 6)")
       print("\t Loops, Lists and List Processing")
       print(50*'-')
@@ -152,10 +166,172 @@ while True:
         continue
       print(50*'-')
   
+  elif pilihMenu == "7":
+    while True:
+      os.system('cls')
+      print(50*'=')
+      print("\t\t  (Pertemuan 7)")
+      print("\t\t     Pra UTS")
+      print(50*'-')
+      print(menus)
+      print(50*'-')
+      pilihMenus = input("Pilih Menu: ")
+      print(50*'-')
+      if pilihMenus == "1":
+        print(Pertemuan7.soal())
+      elif pilihMenus == "2":
+        print(Pertemuan7.tugas())
+      elif pilihMenus == "3":
+        os.system('cls')
+        break
+      else:
+        print("Menu tidak ditemukan!")
+        continue
+      print(50*'-')
+  
+  elif pilihMenu == "8":
+    while True:
+      os.system('cls')
+      print(50*'=')
+      print("\t\t  (Pertemuan 8)")
+      print("\t\t       UTS")
+      print(50*'-')
+      print(menus)
+      print(50*'-')
+      pilihMenus = input("Pilih Menu: ")
+      print(50*'-')
+      if pilihMenus == "1":
+        print(Pertemuan8.soal())
+      elif pilihMenus == "2":
+        print(Pertemuan8.tugas())
+      elif pilihMenus == "3":
+        os.system('cls')
+        break
+      else:
+        print("Menu tidak ditemukan!")
+        continue
+      print(50*'-')
+  
+  elif pilihMenu == "9":
+    while True:
+      os.system('cls')
+      print(50*'=')
+      print("\t\t  (Pertemuan 9)")
+      print("\t\t    Prosedur")
+      print(50*'-')
+      print(menus)
+      print(50*'-')
+      pilihMenus = input("Pilih Menu: ")
+      print(50*'-')
+      if pilihMenus == "1":
+        print(Pertemuan9.soal())
+      elif pilihMenus == "2":
+        print(Pertemuan9.tugas())
+      elif pilihMenus == "3":
+        os.system('cls')
+        break
+      else:
+        print("Menu tidak ditemukan!")
+        continue
+      print(50*'-')
+  
+  elif pilihMenu == "10":
+    while True:
+      os.system('cls')
+      print(50*'=')
+      print("\t\t  (Pertemuan 10)")
+      print("\t\tFungsi dan Parameter")
+      print(50*'-')
+      print(menus)
+      print(50*'-')
+      pilihMenus = input("Pilih Menu: ")
+      print(50*'-')
+      if pilihMenus == "1":
+        print(Pertemuan10.soal())
+      elif pilihMenus == "2":
+        print(Pertemuan10.tugas())
+      elif pilihMenus == "3":
+        os.system('cls')
+        break
+      else:
+        print("Menu tidak ditemukan!")
+        continue
+      print(50*'-')
+  
+  elif pilihMenu == "11":
+    while True:
+      os.system('cls')
+      print(50*'=')
+      print("\t\t  (Pertemuan 11)")
+      print("    Array Manipulation: Sorting dan Searching")
+      print(50*'-')
+      print(menus)
+      print(50*'-')
+      pilihMenus = input("Pilih Menu: ")
+      print(50*'-')
+      if pilihMenus == "1":
+        print(Pertemuan11.soal())
+      elif pilihMenus == "2":
+        print(Pertemuan11.tugas())
+      elif pilihMenus == "3":
+        os.system('cls')
+        break
+      else:
+        print("Menu tidak ditemukan!")
+        continue
+      print(50*'-')
+  
+  elif pilihMenu == "12":
+    while True:
+      os.system('cls')
+      print(50*'=')
+      print("\t\t  (Pertemuan 12)")
+      print("\t   Exceptions dan Data Processing")
+      print(50*'-')
+      print(menus)
+      print(50*'-')
+      pilihMenus = input("Pilih Menu: ")
+      print(50*'-')
+      if pilihMenus == "1":
+        print(Pertemuan12.soal())
+      elif pilihMenus == "2":
+        print(Pertemuan12.tugas())
+      elif pilihMenus == "3":
+        os.system('cls')
+        break
+      else:
+        print("Menu tidak ditemukan!")
+        continue
+      print(50*'-')
+  
+  elif pilihMenu == "13":
+    while True:
+      os.system('cls')
+      print(50*'=')
+      print("\t\t  (Pertemuan 13)")
+      print("\t\tStruktur Data List")
+      print(50*'-')
+      print(menus)
+      print(50*'-')
+      pilihMenus = input("Pilih Menu: ")
+      print(50*'-')
+      if pilihMenus == "1":
+        print(Pertemuan13.soal())
+      elif pilihMenus == "2":
+        print(Pertemuan13.tugas())
+      elif pilihMenus == "3":
+        os.system('cls')
+        break
+      else:
+        print("Menu tidak ditemukan!")
+        continue
+      print(50*'-')
+  
   elif pilihMenu == "q":
     os.system('cls')
     quit()
   
   else:
+    os.system('cls')
     print("Menu tidak ditemukan!")
     continue
